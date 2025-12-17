a64bdca5d9ed35ebb0d3e122398410a242b99f76
Diego Pereira <diego.pereira@fortytwo.io>
2025-12-17 17:50:06 +0000

    update worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'air-dodge-v1';
+const CACHE_NAME = 'air-dodge-v2';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -9,12 +9,29 @@ const urlsToCache = [
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+        .then((cache) => cache.addAll(urlsToCache))
+        .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    Promise.all([
+      clients.claim(),
+      caches.keys().then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+        )
+      )
+    ])
   );
 });
